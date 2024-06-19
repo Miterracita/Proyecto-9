7e86db6570e3ee89e063c51eb8c6315f89817360
Ana <102660581+Miterracita@users.noreply.github.com>
2024-06-19 11:05:07 +0200

    revisión código

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const scrapeProducts = async () => {
     });
 
     const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
 
     try {
         await page.goto(url, { timeout: 90000 });
@@ -36,54 +37,49 @@ const scrapeProducts = async () => {
     let pageNumber = 1;
 
     while (hasNextPage) {
-        try {
-            console.log(`Scraping page ${pageNumber}...`);
 
-            // Esperar a que los productos se carguen en la página
+        console.log(`Scraping page ${pageNumber}...`);
+
+        // Esperar a que los productos se carguen en la página
+        await page.waitForSelector(".product-card div.product-card__content h3.product-card__title", { timeout: 30000 });
+
+        const title = await page.$$eval(".product-card div.product-card__content h3.product-card__title", (nodes) => 
+            nodes.map((n) => n.innerText)
+        );
+
+        const price = await page.$$eval(".product-card__price-container span", (nodes) => 
+            nodes.map((n) => n.innerText)
+        );
+
+        const img = await page.$$eval(".product-card__img-container img", (nodes) => 
+            nodes.map((n) => n.getAttribute("src"))
+        );
+
+        const pccomponentesPortatiles = title.map((value, index) => {
+            return {
+                title: title[index],
+                price: price[index],
+                img: img[index]
+            };
+        });
+
+        allProducts = allProducts.concat(pccomponentesPortatiles);
+
+        const nextPageButton = await page.$('[aria-label="Página siguiente"]');
+        
+        if (nextPageButton) {
+            console.log("Haciendo clic para ir a la siguiente página...");
+            await Promise.all([
+                nextPageButton.click(),
+                page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 90000 })
+            ]);
+
+            // Esperar a que los productos de la nueva página se carguen
             await page.waitForSelector(".product-card div.product-card__content h3.product-card__title", { timeout: 30000 });
 
-            const title = await page.$$eval(".product-card div.product-card__content h3.product-card__title", (nodes) => 
-                nodes.map((n) => n.innerText)
-            );
-
-            const price = await page.$$eval(".product-card__price-container span", (nodes) => 
-                nodes.map((n) => n.innerText)
-            );
-
-            const img = await page.$$eval(".product-card__img-container img", (nodes) => 
-                nodes.map((n) => n.getAttribute("src"))
-            );
-
-            const pccomponentesPortatiles = title.map((value, index) => {
-                return {
-                    title: title[index],
-                    price: price[index],
-                    img: img[index]
-                };
-            });
-
-            allProducts = allProducts.concat(pccomponentesPortatiles);
-
-            const nextPageButton = await page.$('[aria-label="Página siguiente"]');
-
-            if (nextPageButton) {
-                console.log("Haciendo clic para ir a la siguiente página...");
-                await Promise.all([
-                    nextPageButton.click(),
-                    page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 90000 }),
-                ]);
-
-                // Esperar a que los productos de la nueva página se carguen
-                await page.waitForSelector(".product-card div.product-card__content h3.product-card__title", { timeout: 30000 });
-
-                console.log(`Página ${pageNumber + 1} cargada correctamente.`);
-                pageNumber++;
-            } else {
-                hasNextPage = false;
-            }
-
-        } catch (error) {
-            console.error('Error during scraping:', error);
+            console.log(`Página ${pageNumber + 1} cargada correctamente.`);
+            pageNumber++;
+        } else {
             hasNextPage = false;
         }
     }
